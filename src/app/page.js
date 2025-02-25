13a2154a3dfe1f1bdd87586244e7050e1d19eab8
kryz3 <pjgirault23@gmail.com>
2025-02-25 21:37:11 +0100

    ^^

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-const description = 'Le Bike & Run de L\'Institut Mines Télécom Evry (IMT) et l\'Institut Polytechnique de Paris (IP Paris) est une course en binôme à pieds et à vélo, organisé sur deux parcours: un parcours "Découverte" d\'environ 7km et un parcours "Avancé" d\'environ 15km, à travers les villes d\'Evry-Courcouronnes et de Soisy-sur-Seine, et il traversera aussi la forêt de Sénart. La participation est ouverte aux étudiants de l\'IMT Evry (Télécom SudParis et Institut Mines Télécom Business School), de l\'Université d\'Evry, et des écoles de l\'IP Paris (Campus de Palaiseau, l\'Ecole des Ponts, etc.).';
+const description =
+  "Le Bike & Run de L'Institut Mines Télécom Evry (IMT) et l'Institut Polytechnique de Paris (IP Paris) est une course en binôme à pieds et à vélo, organisé sur deux parcours: un parcours \"Découverte\" d'environ 7km et un parcours \"Avancé\" d'environ 15km, à travers les villes d'Evry-Courcouronnes et de Soisy-sur-Seine, et il traversera aussi la forêt de Sénart. La participation est ouverte aux étudiants de l'IMT Evry (Télécom SudParis et Institut Mines Télécom Business School), de l'Université d'Evry, et des écoles de l'IP Paris (Campus de Palaiseau, l'Ecole des Ponts, etc.).";
 
+export const metadata = {
+  title: "Accueil - Bike&Run",
+};
 
-  export default function Home() {
-    return (
-      <div>
-      </div>
-    );
-  }
- 
+export default function Home() {
+  return <div></div>;
+}
